ab2ed8601436e3765f04844462b24f5afde15deb
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-27 17:14:15 +0530

    feat: Added husky

--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,12 @@
 const button = document.getElementById('changeColor');
 
 button.onclick = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {
-                code: 'document.body.className = "dark web"',
-            }
-        );
-    });
-}
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {
+        code: 'document.body.className = "dark web"',
+      },
+    );
+  });
+};
