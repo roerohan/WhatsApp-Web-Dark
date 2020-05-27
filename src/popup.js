07645cee040e2eb8c6f0a7b05975c6eea556129e
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-27 17:07:25 +0530

    fix: Removed unnecessary log

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,6 @@
 const button = document.getElementById('changeColor');
 
 button.onclick = () => {
-    console.log('Hello');
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
